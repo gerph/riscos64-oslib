5d6945dd485e769c75ff0051732a15b7e16e5feb
Charles Ferguson <gerph@gerph.org>
2025-08-16 01:27:24 +0100

    Fix for the __swi replacements breaking the NOT_USED macro.
    The __swi replacements were breaking the NOT_USED macro - it commented
    out until a ';' was encountered, so the defines in types.h became
    broken. This should fix the issue.

--- create_hfiles.sh
+++ create_hfiles.sh
@@ -11,8 +11,8 @@ for i in oslib/include/*.h oslib/Macros/header oslib/Types/header ; do
     elif [[ "$i" == 'oslib/Types/header' ]] ; then
         name="types.h"
     fi
-    env LC_ALL=C sed -E -e '/__swi[^;]*$/,/;/ s/^/\/\//' \
-                        -e '/__swi.*;$/ s/^/\/\//' \
+    env LC_ALL=C sed -E -e '/^__swi[^;]*$/,/;/ s/^/\/\//' \
+                        -e '/^__swi.*;$/ s/^/\/\//' \
                         -e 's/([ (])int /\1int32_t /' \
                         -e 's/unsigned int32_t/uint32_t/' \
                         -e 's/typedef int32_t *bool;/#include <stdbool.h>/' \
